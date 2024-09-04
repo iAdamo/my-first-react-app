fccd32c3147560cfa2de6bcf9b35e85e12304ed9
Adam_Babatunde_Sanusi <iadamo.inc@gmail.com>
2024-09-04 22:06:08 +0100

    Using Prop Types for Type Checking

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import PropTypes from 'prop-types'
 function UserInfo(props) {
-    return (
-      <div>
-        <h3>User Information</h3>
-        <p>Name: {props.name}</p>
-        <p>Email: {props.email}</p>
-        <p>Age: {props.age}</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>User Information</h3>
+      <p>Name: {props.name}</p>
+      <p>Email: {props.email}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+}
+
+UserInfo.defaultProps = {
+  name: 'Guest',
+  email: 'guest@example.com',
+  age: 'N/A',
+};
+
+UserInfo.propTypes = {
+  name: PropTypes.string,
+  email: PropTypes.string,
+  age: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
   export default UserInfo;
