a51318ecbab1a25d426240c5de953a6d5e1469aa
Adam_Babatunde_Sanusi <iadamo.inc@gmail.com>
2024-09-04 11:30:30 +0100

    Handling User Input - Adding an Input Field to App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ function App() {
   // Declare a new state variable called "name"
   const [name, setName] = useState('Adam');
 
+  // Handle user input
+  function handleInputChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My First React App!</h1>
         <p>This is my first React component.</p>
         <Welcome name={name} /> {/* Add a Welcome component */}
-        <button onClick={() => setName('Tunde')}>Change Name</button>
+        <input type="text" value={name} onChange={handleInputChange} placeholder='Enter your name' />
+        {/* <button onClick={() => setName('Tunde')}>Change Name</button> */}
       </header>
     </div>
   );
