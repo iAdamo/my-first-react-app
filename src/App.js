2f42d6f6801a98368697c93c779a66e44a494fdc
Adam_Babatunde_Sanusi <iadamo.inc@gmail.com>
2024-09-04 10:18:26 +0100

    Introduction to State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 import Welcome from './Welcome';
 
 
 function App() {
+  // Declare a new state variable called "name"
+  const [name, setName] = useState('Adam');
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My First React App!</h1>
         <p>This is my first React component.</p>
-        <Welcome name="Adam" /> {/* Add a Welcome component */}
+        <Welcome name={name} /> {/* Add a Welcome component */}
+        <button onClick={() => setName('Tunde')}>Change Name</button>
       </header>
     </div>
   );
